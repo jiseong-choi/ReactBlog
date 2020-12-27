9e8076a96fb66164e218e13e40adc5cd9eb8ba12
jiseong-choi <jiseong1951@kakao.com>
2020-12-27 17:35:16 +0900

    ok

--- client/src/components/views/LandingPage/LadingPage.js
+++ client/src/components/views/LandingPage/LadingPage.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom'
+import { message } from 'antd';
 
 function LadingPage(props){
 
-
-    React.useEffect(() => {
-        axios.get('/api/hello')
-            .then(response => console.log(response))
-    }, [])
+    useEffect(() => {
+        axios.get('')
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response.data)
+                } else {
+                    message.error('글을 불러오지못했습니다.')
+                }
+            })
+    },[])
 
     return(
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height:'90vh' }}>
-            <h2>
-                시작 페이지
-            </h2>
+            
         </div>
     )
 }
